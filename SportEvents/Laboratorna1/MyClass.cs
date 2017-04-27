b1023d6cb86d9a6071d128e5679daf47d0031926
yuliazhydchenko <juliazhydchenko@gmail.com>
2017-04-27 09:47:16 +0300

    first commit

---
+++ SportEvents/Laboratorna1/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorna1
+{
+    class MyClass
+    {
+
+        public static void main(String[] args) {
+          
+            
+           
+
+
+        }
+    }
+}
